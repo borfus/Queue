0881f7f741e2e25d68d7654852ad952a64ceb3f9
borfus <mrpeteyb@gmail.com>
2018-03-20 03:17:02 -0500

    Changed ResizingArrayQueue.java

--- ResizingArrayQueue.java
+++ ResizingArrayQueue.java
@@ -1,12 +1,49 @@
+/******************************************************************************
+ *  Compilation:  javac ResizingArrayQueue.java
+ *  Execution:    java ResizingArrayQueue < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/13stacks/tobe.txt  
+ *  
+ *  Queue implementation with a resizing array.
+ *
+ *  % java ResizingArrayQueue < tobe.txt 
+ *  to be or not to be (2 left on queue)
+ *
+ ******************************************************************************/
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ *  The {@code ResizingArrayQueue} class represents a first-in-first-out (FIFO)
+ *  queue of generic items.
+ *  It supports the usual <em>enqueue</em> and <em>dequeue</em>
+ *  operations, along with methods for peeking at the first item,
+ *  testing if the queue is empty, and iterating through
+ *  the items in FIFO order.
+ *  <p>
+ *  This implementation uses a resizing array, which double the underlying array
+ *  when it is full and halves the underlying array when it is one-quarter full.
+ *  The <em>enqueue</em> and <em>dequeue</em> operations take constant amortized time.
+ *  The <em>size</em>, <em>peek</em>, and <em>is-empty</em> operations takes
+ *  constant time in the worst case. 
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
 public class ResizingArrayQueue<Item> implements Iterable<Item> {
     private Item[] q;       // queue elements
     private int n;          // number of elements on queue
     private int first;      // index of first element of queue
     private int last;       // index of next available slot
 
+
+    /**
+     * Initializes an empty queue.
+     */
     public ResizingArrayQueue() {
         q = (Item[]) new Object[2];
         n = 0;
@@ -14,14 +51,23 @@ public class ResizingArrayQueue<Item> implements Iterable<Item> {
         last = 0;
     }
 
+    /**
+     * Is this queue empty?
+     * @return true if this queue is empty; false otherwise
+     */
     public boolean isEmpty() {
         return n == 0;
     }
 
+    /**
+     * Returns the number of items in this queue.
+     * @return the number of items in this queue
+     */
     public int size() {
         return n;
     }
 
+    // resize the underlying array
     private void resize(int capacity) {
         assert capacity >= n;
         Item[] temp = (Item[]) new Object[capacity];
@@ -33,6 +79,10 @@ public class ResizingArrayQueue<Item> implements Iterable<Item> {
         last  = n;
     }
 
+    /**
+     * Adds the item to this queue.
+     * @param item the item to add
+     */
     public void enqueue(Item item) {
         // double size of array if necessary and recopy to front of array
         if (n == q.length) resize(2*q.length);   // double size of array if necessary
